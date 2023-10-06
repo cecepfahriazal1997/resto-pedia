5c31d070e5d84e2cccb4027133522b16ee227fb9
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-10-06 16:54:36 +0700

    Add footer section

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -9,11 +9,8 @@ class Footer extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <footer>
-            <div class="container">
-                <div>@2023 Cecep Rokani All Served.</div>
-            </div>
-        </footer>`;
+            <div class="border-footer"></div>
+            <div class="content-footer">Copyright 	&#169; 2023 - Cecep Rokani All Rights Reserved.</div>`;
     }
 }
 
