c46bbc0c3375e7de100d921983af991ac95a96be
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-10-08 14:12:18 +0700

    Add search on hero

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -13,6 +13,10 @@ class Hero extends HTMLElement {
             <h1>RestoPedia</h1>
             <p id="subtitle">Semua Restaurant Nusantara Ada Disini.</p>
             <p id="description">Yuk! cari restaurant favoritmu.</p>
+            <div class="search">
+                <input type="text" class="form" placeholder="Cari restaurant disini..."></input>
+                <span class="ph ph-magnifying-glass"></span>
+            </div>
         </div>
         `;
     }
