457db69dcf0e46394c02813c3959b968ed5a5b7c
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-10-19 14:56:44 +0700

    Custom port dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
-    port: 9999,
+    port: 1234,
     client: {
       overlay: {
         errors: true,
