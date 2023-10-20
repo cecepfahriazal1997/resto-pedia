c9a19b26904ef93204d51bd86760d85f1267bd1e
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-10-20 16:51:06 +0700

    Update service worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,33 +1,36 @@
-import 'regenerator-runtime';
-import CacheHelper from './utils/cache-helper';
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute, Route } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import CONFIG from './globals/config';
 
-// Caching the listed asset below
-const assetsToCache = [
-    './',
-    './images/icons/icon-72x72.png',
-    './images/icons/icon-96x96.png',
-    './images/icons/icon-128x128.png',
-    './images/icons/icon-144x144.png',
-    './images/icons/icon-152x152.png',
-    './images/icons/icon-192x192.png',
-    './images/icons/icon-384x384.png',
-    './images/icons/icon-512x512.png',
-    './index.html',
-    './logo.png',
-    './user.png',
-    './app.bundle.js',
-    './app.webmanifest',
-    './sw.bundle.js',
-];
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+const themoviedbApi = new Route(
+    ({
+        url
+    }) => url.href.startsWith(CONFIG.API_BASE_URL),
+    new StaleWhileRevalidate({
+        cacheName: 'restaurant-api',
+    }),
+);
+
+registerRoute(themoviedbApi);
+
+self.addEventListener('install', () => {
+    console.log('Service Worker: Installed');
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(CacheHelper.deleteOldCache());
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(CacheHelper.revalidateCache(event.request));
-});
+// self.addEventListener('push', (event) => {
+//     const dataJson = event.data.json();
+//     const notification = {
+//         title: dataJson.title,
+//         options: {
+//             body: dataJson.options.body,
+//             icon: dataJson.options.icon,
+//             image: dataJson.options.image,
+//         },
+//     };
+//     event.waitUntil(self.registration.showNotification(notification.title, notification.options));
+// });
